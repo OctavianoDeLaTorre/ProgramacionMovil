55f9463ca65bb720703f9d3f3243fd0aebec5529
Octaviano De La Torre <octavianodelatorre@gmail.com>
2019-02-19 17:03:26 -0600

    Creación del proyecto Loader

--- SearchView/app/src/main/java/com/octaviano/searchview/MainActivity.java
+++ SearchView/app/src/main/java/com/octaviano/searchview/MainActivity.java
@@ -2,20 +2,26 @@ package com.octaviano.searchview;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView etiqueta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etiqueta = findViewById(R.id.etiqueta);
     }
 
     @Override
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         MenuItem searchItem = menu.findItem(R.id.menu_search);
         SearchView searchView = (SearchView) searchItem.getActionView();
+
+
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
--- SearchView/app/src/main/java/com/octaviano/searchview/SearchResultActivity.java
+++ SearchView/app/src/main/java/com/octaviano/searchview/SearchResultActivity.java
@@ -2,11 +2,13 @@ package com.octaviano.searchview;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SearchResultActivity extends AppCompatActivity {
+
 
     TextView mTextViewSearchResult;
 
@@ -14,6 +16,7 @@ public class SearchResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
         mTextViewSearchResult = findViewById(R.id.textViewSearchResult);
 
         if (Intent.ACTION_SEARCH.equals(getIntent().getAction())) {

1d25bd23cec88eba41505a39a15dd005b51d3468
Octaviano De La Torre <octavianodelatorre@gmail.com>
2019-03-04 23:31:53 -0600

    Creacion del proyecto PressEvents

--- OpenCV/app/src/main/java/com/octaviano/opencv/AnalisisIMG.java
+++ OpenCV/app/src/main/java/com/octaviano/opencv/AnalisisIMG.java
@@ -1,10 +1,14 @@
 package com.octaviano.opencv;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.CLAHE;
 import org.opencv.imgproc.Imgproc;
 
 
@@ -15,7 +19,84 @@ import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnalisisIMG {
+
+    public Bitmap segmentar(Bitmap bmp){
+        Mat newimg = new Mat();
+        Bitmap bmp32 = bmp.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bmp32, newimg);
+        Mat des = new Mat(newimg.rows(), newimg.cols(), newimg.type());
+        Mat norm = new Mat();
+
+        Imgproc.cvtColor(newimg, des, Imgproc.COLOR_BGR2HSV);
+        List<Mat> hsv = new ArrayList<Mat>();
+        Core.split(des, hsv);
+        Mat v = hsv.get(2); //gets the grey scale version
+
+        //Imgcodecs.imwrite("/Users/Lisa-Maria/Documents/CapturedImages/B&Wpic.jpg", v); //only writes mats
+
+        CLAHE clahe = Imgproc.createCLAHE(2.0, new Size(8,8) ); //2.0, new Size(8,8)
+        clahe.apply(v,v);
+//    Imgproc.GaussianBlur(v, v, new Size(9,9), 3); //adds left pupil boundary and random circle on 'a'
+        //   Imgproc.GaussianBlur(v, v, new Size(9,9), 13); //adds right outer iris boundary and random circle on 'a'
+        Imgproc.GaussianBlur(v, v, new Size(9,9), 7);  //adds left outer iris boundary and random circle on left by hair
+        //  Imgproc.GaussianBlur(v, v, new Size(7,7), 15);
+        Core.addWeighted(v, 1.5, v, -0.5, 0, v);
+
+
+
+        if (v != null) {
+
+            Mat circles = new Mat();
+            Imgproc.HoughCircles( v, circles, Imgproc.CV_HOUGH_GRADIENT, 2, v.rows(), 100, 20, 20, 200 );
+
+            List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+            System.out.println("circles.cols() " + circles.cols());
+            if(circles.cols() > 0) {
+                System.out.println("1");
+                for (int x = 0; x < circles.cols(); x++) {
+                    System.out.println("2");
+                    double vCircle[] = circles.get(0, x);
+
+
+                    if(vCircle == null) {
+                        break;
+                    }
+
+                    Point pt = new Point(Math.round(vCircle[0]), Math.round(vCircle[1]));
+                    int radius = (int) Math.round(vCircle[2]);
+
+                    //draw the found circle
+
+
+
+
+                    Imgproc.circle(v, pt, radius, new Scalar(255,0,0),2); //newimg
+                    //Imgproc.circle(des, pt, radius/3, new Scalar(225,0,0),2); //pupil
+                    Imgcodecs.imwrite("/Users/.../Houghpic.jpg", v); //newimg
+
+                    //draw the mask: white circle on black background
+//                  Mat mask = new Mat( new Size( des.cols(), des.rows() ), CvType.CV_8UC1 );
+//                  Imgproc.circle(mask, pt, radius, new Scalar(255,0,0),2);
+
+//                  des.copyTo(des,mask);
+//                  Imgcodecs.imwrite("/Users/..../mask.jpg", des); //newimg
+
+                    Imgproc.logPolar(des, norm, pt, radius, Imgproc.WARP_FILL_OUTLIERS);
+
+
+                }
+            }
+        }
+
+        Utils.matToBitmap(v, bmp);
+        return bmp;
+    }
+
 
     public Bitmap buscarCirculos(Bitmap bmp) {
         Mat src = new Mat();
@@ -63,7 +144,7 @@ public class AnalisisIMG {
             Imgproc.circle(src, center, radius, new Scalar(0, 0, 255), 3, 8, 0);
         }
 
-        Utils.matToBitmap(src_gray, bmp);
+        Utils.matToBitmap(src, bmp);
         return bmp;
     }
 
